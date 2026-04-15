ec6e1405fd19ea2851f469b352b27fc6ffd5aabe
vadim <vadimm87@mail.ru>
2026-04-15 20:47:21 +0300

    исправить фикстуры

--- conftest.py
+++ conftest.py
@@ -9,26 +9,10 @@ from utils.allure_helpers import attach_request_data, attach_response
 def courier_client():
     return CourierClient()
 
-
-@pytest.fixture
-def registered_courier(courier_client):
-    data = generate_courier_data()
-    response = courier_client.create_courier(data)
-    assert response.status_code == 201
-    # Получаем id курьера через логин
-    login_resp = courier_client.login_courier(data["login"], data["password"])
-    courier_id = login_resp.json()["id"]
-    
-    yield data  # тест выполняется
-    
-    # Удаляем курьера после теста
-    courier_client.delete_courier(courier_id)
-
-
 @pytest.fixture
 def create_courier_response(courier_client):
     #Фикстура для создания курьера с логированием в Allure.
-        with allure.step('Генерация данных нового курьера'):
+        with allure.step('Сгнерироать данные для нового курьера'):
             data = generate_courier_data()
             attach_request_data(data, name='Данные курьера')
 
@@ -36,4 +20,21 @@ def create_courier_response(courier_client):
             response = courier_client.create_courier(data)
             attach_response(response, name_prefix="Создание курьера")
         return response, data
-        
+
+@pytest.fixture
+def registered_courier(courier_client, create_courier_response):
+    #Фикстура для создания курьера с логированием в Allure и удаление созданного курьера в конце теста
+    response, data = create_courier_response
+    
+    with allure.step('Провеить успешное создание курьера'):    
+        assert response.status_code == 201
+    
+    with allure.step('Получить id курьера через логин'):   
+        login_resp = courier_client.login_courier(data["login"], data["password"])
+        courier_id = login_resp.json()["id"]
+    
+    yield data  
+    
+    with allure.step('Удалить созданного курьера'):
+        courier_client.delete_courier(courier_id)
+
